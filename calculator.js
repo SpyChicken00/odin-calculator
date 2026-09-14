dbb0b9c8d64d5770a058d99dba306edde395f2f8
SpyChicken00 <94644105+SpyChicken00@users.noreply.github.com>
2026-09-14 17:19:46 +0900

    Add basic functionality for clear, 123, + =

--- calculator.js
+++ calculator.js
@@ -16,6 +16,7 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     //need support for floats?
     //dont allow dividing by 0, check here or later?
+    if (num2 === 0) return "Impossible Silly Goose!"
     return num1 / num2
 }
 
@@ -24,23 +25,86 @@ function divide(num1, num2) {
 function operate(num1, operator, num2) {
     switch (operator) {
         case "+":
-            add(num1, num2)
-            break;
+            return add(num1, num2)
         case "-":
-            subtract(num1, num2)
-            break;
+            return subtract(num1, num2)
         case "*":
-            multiply(num1, num2)
-            break;
+            return multiply(num1, num2)
         case "/":
-            divide(num1, num2)
-            break;
+           return  divide(num1, num2)
         
     }
 }
 
+function removeHighlight() {
+    operatorButtons.map(button => button.style.border = "none")
+}
+
+//called when the user preses a button
+function updateNum(e) {
+    removeHighlight()
+
+
+    currNumberP.innerText += e.target.textContent
+    //update calculator value 
+    if (operator === "") {
+        firstNum = parseInt(currNumberP.innerText)
+    } else {
+        secondNum = parseInt(currNumberP.innerText)
+    }
+}
+
+function updateOperator(e) {
+    removeHighlight()
+
+    operator = e.target.textContent
+    e.target.style.border = "solid 3px white"
+    currNumberP.innerText = ""
+}
+
+function equalsCalc() {
+    currentResult = operate(firstNum, operator, secondNum)
+    currNumberP.innerText = currentResult
+    prevResultP.innerText = currentResult
+    firstNum = currentResult
+    secondNum = 0
+}
+
+function clearCalc() {
+    firstNum = 0;
+    secondNum = 0;
+    currentResult = 0;
+    operator = ""
+    currNumberP.innerText = ""
+    prevResultP.innerText = "";
+    removeHighlight()
+}
+
+
+const currNumberP = document.querySelector("#currentNumber")
+const prevResultP = document.querySelector("#prevTotal")
+
+const clearButton = document.querySelector("#clear")
+
+const equalsButton = document.querySelector("#equals")
+const plusButton = document.querySelector("#add")
+const operatorButtons = [equalsButton, plusButton]
+
+const oneButton = document.querySelector("#one")
+const twoButton = document.querySelector("#two")
+const threeButton = document.querySelector("#three")
+const numButtons = [oneButton, twoButton, threeButton]
+
+clearButton.addEventListener("click", clearCalc)
+equalsButton.addEventListener("click", equalsCalc)
+plusButton.addEventListener("click", updateOperator)
+
+
+numButtons.map((button) => {
+    button.addEventListener("click", updateNum)
+})
 
 let firstNum = 0;
-let operator = "+"
+let operator = ""
 let secondNum = 0;
 let currentResult = 0;
